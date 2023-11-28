5b07734326126d8fa4e84c8479fc12b61ea55129
Felipe <felipetermometria@hotmail.com>
2023-11-28 16:27:27 -0300

    Initial commit

---
+++ app/src/main/java/com/example/app_estudo/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.app_estudo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.app_estudo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            val nome: String = binding.editNome.text.toString()
+            //binding.textResultado.text = "Ola" + nome
+            binding.textResultado.text = "Ola" + nome
+            //binding.textResultado.text = "Ola" + nome
+        }
+    }
+}
